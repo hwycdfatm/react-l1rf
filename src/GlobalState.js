0d17316139545edc3c6847a36207fc3a62c4994e
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2021-06-27 22:13:40 +0700

    Change Something

---
+++ src/GlobalState.js
@@ -0,0 +1,7 @@
+import React, { createContext, useState } from 'react'
+
+export const GlobalSate = createContext()
+
+export const DataProvider = ({ children }) => {
+	return <GlobalSate.Provider value={'Value'}>{children}</GlobalSate.Provider>
+}
---
+++ src/components/MainPage/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+	return (
+		<div>
+			Error
+			<span></span>
+		</div>
+	)
+}
+
+export default Error
