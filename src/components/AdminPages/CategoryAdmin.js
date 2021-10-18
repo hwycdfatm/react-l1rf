f762f826bcdeafa9bd76aa3cabf51db7b14df0f5
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2021-10-18 16:58:31 +0700

    lỗi quài

--- src/components/AdminPages/CategoryAdmin.js
+++ src/components/AdminPages/CategoryAdmin.js
@@ -3,17 +3,14 @@ import productAPI from '../../api/productAPI'
 import Form from './Form'
 import { GlobalState } from '../../GlobalState'
 import { useDetectOutsideClick } from '../../utils/useDetectOutsideClick'
-import Pagination from '../../utils/Pagination'
+
 const CategoryAdmin = () => {
 	const { token, categories } = useContext(GlobalState)
 	const [productList, setProductList] = useState([])
 
-	const [currentPage, setCurrentPage] = useState(0)
 	const [product, setProduct] = useState({})
 
 	const [visible, setVisible] = useState(false)
-
-	const [totalPage, setTotalPage] = useState(0)
 
 	const [filterCategory, setFilterCategory] = useState('Tất cả')
 
@@ -27,20 +24,19 @@ const CategoryAdmin = () => {
 			try {
 				const params = {
 					category: filterCategory !== 'Tất cả' ? filterCategory : '',
-					_page: currentPage,
-					_limit: 20, // tối đa bao nhiêu sản phẩm trong 1 trang
+
+					_limit: 1000, // tối đa bao nhiêu sản phẩm trong 1 trang
 				}
 				const result = await productAPI.getAll(params)
 				if (result.status === 'Success') {
 					setProductList(result.data)
-					setTotalPage(result.pagination._total_Page)
 				}
 			} catch (err) {
 				console.log(err)
 			}
 		}
 		fetchProduct()
-	}, [filterCategory, currentPage])
+	}, [filterCategory])
 
 	useEffect(() => {
 		setVisible(false)
@@ -71,13 +67,17 @@ const CategoryAdmin = () => {
 	const typeingTimeoutRef = useRef(null)
 
 	const handleSubmitSearch = async (value) => {
-		const params = {
-			q: value,
-			_limit: 10,
-		}
-		const result = await productAPI.getAll(params)
-		if (result.status === 'Success') {
-			setResultSearchValue(result.data)
+		if (value !== ' ' && value) {
+			const params = {
+				q: value,
+				_limit: 10,
+			}
+			const result = await productAPI.getAll(params)
+			if (result.status === 'Success') {
+				setResultSearchValue(result.data)
+			}
+		} else {
+			setResultSearchValue([])
 		}
 	}
 
@@ -177,85 +177,93 @@ const CategoryAdmin = () => {
 								className="flex-1 p-1 text-sm bg-transparent focus:outline-none focus:shadow-outline"
 							/>
 						</div>
-
-						{searchValue && (
-							<div className="absolute bg-white rounded-md h-auto shadow-lg top-10 left-0 lg:left-8 right-0">
-								{resultSearchValue && resultSearchValue.length >= 1 ? (
-									<div className="flex flex-col space-y-2">
-										{resultSearchValue.map((product) => (
-											<div key={product._id} className="w-full h-16 flex">
-												<img
-													src={product.images[0].url}
-													alt={product.title}
-													className="w-14 h-14"
-												/>
-												<div>
-													<p>{product.title}</p>
-													<span>
-														{parseInt(product.price).toLocaleString('en')}vnđ
-													</span>
-												</div>
-											</div>
-										))}
-									</div>
-								) : (
-									<div> Không tìm thấy</div>
-								)}
-							</div>
-						)}
 					</div>
 				</div>
 				<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-					{productList.map((product, index) => (
-						<div
-							key={index}
-							title={product.title}
-							className="rounded-lg shadow-lg hover:shadow-xl transition duration-700 border overflow-hidden relative"
-						>
-							<input
-								type="checkbox"
-								className="w-4 h-4 absolute top-1 right-1"
-							/>
-							<div className="bg-white">
-								<img
-									src={product.images[0]?.url}
-									alt={product.title}
-									className="w-full object-cover h-48"
-								/>
-								<div className="flex flex-col space-y-1 py-1 px-2">
-									<span className="text-sm font-medium text-gray-500 truncate">
-										{product.title}
-									</span>
-									<span className="text-xs font-medium text-gray-500">
-										{parseInt(product.price).toLocaleString('en')} vnđ
-									</span>
-									<div className="flex p-1 justify-center item-center space-x-3">
-										<button
-											onClick={() => handleShowFormEdit(product._id)}
-											className="bg-yellow-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
-										>
-											Sửa
-										</button>
-										<button
-											onClick={() => handleDelete(product._id)}
-											className="bg-red-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
-										>
-											Xóa
-										</button>
+					{resultSearchValue.length > 1
+						? resultSearchValue.map((product, index) => (
+								<div
+									key={index}
+									title={product.title}
+									className="rounded-lg shadow-lg hover:shadow-xl transition duration-700 border overflow-hidden relative"
+								>
+									<input
+										type="checkbox"
+										className="w-4 h-4 absolute top-1 right-1"
+									/>
+									<div className="bg-white">
+										<img
+											src={product.images?.url}
+											alt={product.title}
+											className="w-full object-cover h-48"
+										/>
+										<div className="flex flex-col space-y-1 py-1 px-2">
+											<span className="text-sm font-medium text-gray-500 truncate">
+												{product.title}
+											</span>
+											<span className="text-xs font-medium text-gray-500">
+												{parseInt(product.price).toLocaleString('en')} vnđ
+											</span>
+											<div className="flex p-1 justify-center item-center space-x-3">
+												<button
+													onClick={() => handleShowFormEdit(product._id)}
+													className="bg-yellow-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
+												>
+													Sửa
+												</button>
+												<button
+													onClick={() => handleDelete(product._id)}
+													className="bg-red-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
+												>
+													Xóa
+												</button>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-					))}
+						  ))
+						: productList.map((product, index) => (
+								<div
+									key={index}
+									title={product.title}
+									className="rounded-lg shadow-lg hover:shadow-xl transition duration-700 border overflow-hidden relative"
+								>
+									<input
+										type="checkbox"
+										className="w-4 h-4 absolute top-1 right-1"
+									/>
+									<div className="bg-white">
+										<img
+											src={product.images?.url}
+											alt={product.title}
+											className="w-full object-cover h-48"
+										/>
+										<div className="flex flex-col space-y-1 py-1 px-2">
+											<span className="text-sm font-medium text-gray-500 truncate">
+												{product.title}
+											</span>
+											<span className="text-xs font-medium text-gray-500">
+												{parseInt(product.price).toLocaleString('en')} vnđ
+											</span>
+											<div className="flex p-1 justify-center item-center space-x-3">
+												<button
+													onClick={() => handleShowFormEdit(product._id)}
+													className="bg-yellow-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
+												>
+													Sửa
+												</button>
+												<button
+													onClick={() => handleDelete(product._id)}
+													className="bg-red-400 w-20 rounded-md font-medium text-white transform hover:-translate-y-1 transition-all"
+												>
+													Xóa
+												</button>
+											</div>
+										</div>
+									</div>
+								</div>
+						  ))}
 				</div>
-				{totalPage > 1 && (
-					<Pagination
-						totalPage={totalPage}
-						currentPage={currentPage}
-						setCurrentPage={setCurrentPage}
-						slug={filterCategory}
-					/>
-				)}
 			</div>
 		</>
 	)
