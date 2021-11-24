efa0eff350e2a01822666f5f544d36242ebfa076
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2021-11-24 21:36:21 +0700

    heheh

--- src/utils/Seo.js
+++ src/utils/Seo.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import Logo from '../images/image-logo.png'
+import Helmet from 'react-helmet'
 
 const Seo = ({
 	title,
 	description = 'Shop bán hàng đầu hàng Việt Nam',
-	image,
+	image = 'https://l1rf.online/image-logo.png',
 }) => {
 	return (
 		<Helmet>
 			<meta name="description" content={description} />
 			<meta property="og:description" content={description} />
 			<meta name="twitter:description" content={description} />
-			<meta property="og:image" content={image ? image : Logo} />
+			<meta property="og:image" content={image} />
 			<meta property="og:image:alt" content="l1rf-store" />
 			<meta property="og:url" content="https://l1rf.online" />
 			<meta property="og:title" content={title} />
+			<meta property="fb:app_id" content="512680796465992" />
+			<meta property="og:type" content="article" />
+			<meta name="author" content="Toàn" />
 			<title>{title}</title>
 		</Helmet>
 	)
